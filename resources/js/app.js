002abb76445684f8c3f433eaa88df8eb2473d806
Daniel Popescu <same.dan@gmail.com>
2022-12-26 18:05:56 +0100

    Channel Chat _= Push Notification

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
 import "./bootstrap";
 import Search from "./live-search";
+import Chat from "./chat";
 
 // If user is logged in, the search icon si visible
 if (document.querySelector(".header-search-icon")) {
     new Search();
+}
+if (document.querySelector(".header-chat-icon")) {
+    new Chat();
 }
